ea49662e0e910fafa21a1bbbf0032c4306fc7383
Ivan Faryga <ifaryga@gmail.com>
2021-07-03 01:10:26 +0300

    added solution of task-05.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,43 +1,5 @@
 const images = [
   {
-<<<<<<< Updated upstream
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
-];
-
-
-const ulREf = document.querySelector('#gallery');
-ulREf.classList.add('gallery');
-
-// map the array and create 
-const galleryBuilder = images => {
-    return images.map(image => 
-        `<li class="gallery__item">
-            <img src="${image.url}" alt="${image.alt}" / width = "450" height = "280">
-    </li>`,
-    );
-};
-
-const gallery = galleryBuilder(images);
-console.log(gallery);
-
-ulREf.insertAdjacentHTML('afterbegin', gallery.join(''));
-
-
-
-
-
-
-=======
     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     alt: "White and Black Long Fur Cat",
   },
@@ -50,4 +12,21 @@ ulREf.insertAdjacentHTML('afterbegin', gallery.join(''));
     alt: "Group of Horses Running",
   },
 ];
->>>>>>> Stashed changes
+
+const ulREf = document.querySelector("#gallery");
+ulREf.classList.add("gallery");
+
+// map the array and create
+const galleryBuilder = (images) => {
+  return images.map(
+    (image) =>
+      `<li class="gallery__item">
+            <img src="${image.url}" alt="${image.alt}" / width = "450" height = "280">
+    </li>`
+  );
+};
+
+const gallery = galleryBuilder(images);
+console.log(gallery);
+
+ulREf.insertAdjacentHTML("afterbegin", gallery.join(""));

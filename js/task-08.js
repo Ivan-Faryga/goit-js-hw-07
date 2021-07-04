bd639c22ee53b9e5d0e2eea7df2522837b825eba
Ivan-Faryga <77795237+Ivan-Faryga@users.noreply.github.com>
2021-07-04 21:43:32 +0300

    updatetask-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -6,9 +6,10 @@ const refs = {
 };
 
 const boxesDivRef = document.querySelector('#boxes');
-console.log(boxesDivRef.style);
+console.dir(boxesDivRef.style);
 
 let inputValue = 0;
+const newBoxesArray = [];
 
 refs.input.addEventListener('input', onInputChange);
 refs.renderBtnRef.addEventListener('click', onRender);
@@ -17,11 +18,18 @@ refs.destroyBtnRef.addEventListener('click', onDestroy);
 
 function onInputChange(event) {
     inputValue = event.currentTarget.value;
+    console.log(inputValue);
 };
+
+console.log(inputValue);
 
 function onRender(inputValue) {
     const newBox = document.createElement('div');
+    newBox.width = `100px`;
+    newBox.height = `100px`;
+    newBoxesArray.push(newBox);
 
+    boxesDivRef.append(...newBoxesArray);
 };
 
 function onDestroy() { };
